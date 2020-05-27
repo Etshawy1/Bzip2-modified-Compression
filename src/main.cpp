e823873a343186b0be7750f6211a0d4bfc39885f
Etshawy1 <muhammad.mahmoud99@eng-st.cu.edu.eg>
2020-05-27 17:31:16 +0200

    more organization in main function

--- src/main.cpp
+++ src/main.cpp
@@ -20,45 +20,18 @@ struct operation
 void usage();
 void copyright();
 operation arg_validations(int argc, char *argv[]);
+string readInput(string filename);
+string execute(bool encoding, string algorithm, string &inputText);
+void writeOutput(string filename, string &outputText);
 
 int main(int argc, char *argv[])
 {
 
   operation o = arg_validations(argc, argv);
-  string inputText = "";
-  string line;
-  ifstream infile;
-  infile.open(o.inputFile, ios::in | ios::binary);
-  if (!infile.is_open())
-  {
-    cout << "couldn't open input file\n";
-    return -1;
-  }
-  while (getline(infile, line))
-    inputText += (line + "\n");
-  inputText.pop_back();
+  string inputText = readInput(o.inputFile);
+  string outputText = execute(o.encoding, o.algorithm, inputText);
+  writeOutput(o.outputFile, outputText);
 
-  string outputText;
-  if (o.encoding)
-  {
-    if (o.algorithm == "bwt")
-      outputText = BWTencode(inputText);
-  }
-  else
-  {
-    if (o.algorithm == "bwt")
-      outputText = BWTdecode(inputText);
-  }
-
-  ofstream ofile;
-  ofile.open(o.outputFile, ios::out | ios::binary);
-  if (!ofile.is_open())
-  {
-    cout << "couldn't open output file\n";
-    return -1;
-  }
-  ofile << outputText;
-  ofile.close();
   if (o.algorithm == "lzw")
     cout << "Compression Ratio = " << inputText.size() * 1.0 / outputText.size() << endl;
   return 0;
@@ -82,22 +55,69 @@ operation arg_validations(int argc, char *argv[])
   {
     usage();
     copyright();
-    exit(0);
+    exit(-2);
   }
 
   if (strcmp(argv[1], "bwt") && strcmp(argv[1], "mft") && strcmp(argv[1], "lzw"))
   {
     usage();
     copyright();
-    exit(0);
+    exit(-2);
   }
 
   if (strcmp(argv[2], "-e") && strcmp(argv[2], "-d"))
   {
     usage();
     copyright();
-    exit(0);
+    exit(-2);
   }
 
   return operation{argv[1], argv[3], argv[4], argv[2][1] == 'e'};
+}
+
+string readInput(string filename)
+{
+  string inputText = "";
+  string line;
+  ifstream infile;
+  infile.open(filename, ios::in | ios::binary);
+  if (!infile.is_open())
+  {
+    cout << "couldn't open input file\n";
+    exit(-1);
+  }
+  while (getline(infile, line))
+    inputText += (line + "\n");
+  inputText.pop_back();
+  infile.close();
+  return inputText;
+}
+
+string execute(bool encoding, string algorithm, string &input)
+{
+  string outputText = "";
+  if (encoding)
+  {
+    if (algorithm == "bwt")
+      outputText = BWTencode(input);
+  }
+  else
+  {
+    if (algorithm == "bwt")
+      outputText = BWTdecode(input);
+  }
+  return outputText;
+}
+
+void writeOutput(string filename, string &outputText)
+{
+  ofstream ofile;
+  ofile.open(filename, ios::out | ios::binary);
+  if (!ofile.is_open())
+  {
+    cout << "couldn't open output file\n";
+    exit(-1);
+  }
+  ofile << outputText;
+  ofile.close();
 }
